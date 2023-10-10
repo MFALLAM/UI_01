42a37b264936ba533b24a0eda6ac0d657d338dd6
MFALLAM <mohammed.fathi.allam@gmail.com>
2023-10-10 20:03:01 +0300

    Initial load

---
+++ app/src/main/java/tweakup/ru/uilover/MainActivity.java
@@ -0,0 +1,14 @@
+package tweakup.ru.uilover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
